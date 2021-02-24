90725c73a0dedd2149f4139d39db4e1652d0e851
Matthias Wolf <matthias.wolf@epfl.ch>
2021-02-24 17:40:39 +0100

    touchdetector: bump to 6.6.6

--- var/spack/repos/builtin/packages/touchdetector/package.py
+++ var/spack/repos/builtin/packages/touchdetector/package.py
@@ -14,6 +14,7 @@ class Touchdetector(CMakePackage):
     git      = "git@bbpgitlab.epfl.ch:hpc/touchdetector.git"
 
     version('develop', submodules=True)
+    version('6.6.6', tag='v6.6.6', submodules=True)
     version('5.5.1', tag='5.5.1', submodules=True)
     version('5.5.0', tag='5.5.0', submodules=True)
     version('5.4.0', tag='5.4.0', submodules=True)
