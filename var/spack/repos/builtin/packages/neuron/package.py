ba986ed2233c50e31a472ddbc386c02ec258855a
Ioannis Magkanaris <iomagkanaris@gmail.com>
2021-06-23 11:36:03 +0200

    Added patch which reverts failing NEURON commit

--- var/spack/repos/builtin/packages/neuron/package.py
+++ var/spack/repos/builtin/packages/neuron/package.py
@@ -30,6 +30,9 @@ class Neuron(CMakePackage):
     # Patch which reverts d9605cb for not hanging on ExperimentalMechComplex
     patch("apply_79a4d2af_load_balance_fix.patch", when="@7.8.0b")
     patch("fix_brew_py_18e97a2d.patch", when="@7.8.0c")
+    # Patch which reverts commit 6a9868e that is failing in the Simulation Stack
+    # See https://github.com/neuronsimulator/nrn/pull/1261#issuecomment-866250715
+    patch("revert_savstatebug_1261.patch", when="@develop")
 
     version("develop", branch="master")
     version("8.0.0", tag="8.0.0", preferred=True)
