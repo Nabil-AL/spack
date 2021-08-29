a542ebaf8f5ee6db70b505d9bb32f69fb51e46fb
Sergio <sergio.rivasgomez@epfl.ch>
2021-08-30 00:50:11 +0200

    Adds HPCTM-1459 package

---
+++ var/spack/repos/builtin/packages/hpctm-1459/package.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+class Hpctm1459(CMakePackage):
+    """HPCTM1459"""
+
+    homepage = "https://bbpteam.epfl.ch/project/issues/browse/HPCTM-1459"
+    url      = "git@bbpgitlab.epfl.ch:hpc/user/hpctm-1459.git"
+    git      = "git@bbpgitlab.epfl.ch:hpc/user/hpctm-1459.git"
+
+    version('develop', branch='main', submodules=False)
+    version('0.1', commit='efcee5', submodules=False)
+
+    depends_on('cmake', type='build')
+
+    def cmake_args(self):
+        return [
+            '-DINTERNAL_PACKAGES:BOOL=OFF'
+        ]
