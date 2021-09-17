17939f1f46c58b8cad1937bd502f837aa41ae875
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-09-17 12:14:12 +0200

    Adjust the py-torch+cuda dependency

--- var/spack/repos/builtin/packages/py-atlinter/package.py
+++ var/spack/repos/builtin/packages/py-atlinter/package.py
@@ -25,7 +25,7 @@ class PyAtlinter(PythonPackage):
 
     # Installation requirements
     depends_on('py-atldld@0.2.2', type=('run'))
-    depends_on('py-torch+cuda+cudnn+nccl', when='+cuda', type=('run'))
+    depends_on('py-torch+cuda', when='+cuda', type=('run'))
     depends_on('py-torch~cuda~cudnn~nccl', when='~cuda', type=('run'))
     depends_on('py-torchvision', type=('run'))
     depends_on('py-numpy', type=('run'))
