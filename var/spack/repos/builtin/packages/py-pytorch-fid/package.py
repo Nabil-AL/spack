1a44bf565aa66b979841df4631cec1566ec8a77a
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-09-17 13:54:53 +0200

    Fix typos and dependencies in py-pytorch-fid

--- var/spack/repos/builtin/packages/py-pytorch-fid/package.py
+++ var/spack/repos/builtin/packages/py-pytorch-fid/package.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack.build_systems.python import PythonPackage
-from spack.directives import depends_on, version
+from spack.directives import depends_on, variant, version
 
 
 class PyPytorchFid(PythonPackage):
@@ -15,11 +15,14 @@ class PyPytorchFid(PythonPackage):
 
     version('0.2.0', sha256='5f3aa23957623fcd0150c467478cdf67b479a2870d570b8c4957d031ac597a11')
 
+    variant('cuda', default=False, description='Enable CUDA support')
+
     depends_on('python@3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
 
-    depends_on('numpy', type('run'))
-    depends_on('pillow', type('run'))
-    depends_on('scipy', type('run'))
-    depends_on('torch@1.0.1:', type('run'))
-    depends_on('torchvision@0.2.2:', type('run'))
+    depends_on('py-numpy', type='run')
+    depends_on('py-pillow', type='run')
+    depends_on('py-scipy', type='run')
+    depends_on('py-torch@1.0.1:+cuda', when='+cuda', type='run')
+    depends_on('py-torch@1.0.1:~cuda~cudnn~nccl', when='~cuda', type='run')
+    depends_on('py-torchvision@0.2.2:', type='run')
